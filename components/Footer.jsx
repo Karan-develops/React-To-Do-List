05657e3e1c08c8bc67eaded3751efa2d42cb829f
Karan Aggarwal <158192204+Karan-develops@users.noreply.github.com>
2024-06-23 18:53:12 +0530

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-purple-600 py-2 text-white font-bold text-lg">
+      <div className="flex justify-around">
+        <span>"Keep pushing forward, one task at a time."</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
